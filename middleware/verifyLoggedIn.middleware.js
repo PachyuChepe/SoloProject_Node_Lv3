c3906e7bcc7b0df89ad651e215c48605418ab00c
PachyuChepe <pachyuchepe@gmail.com>
2023-11-11 23:17:09 +0900

    Feat: 스웨거 api & 회원 CRUD 추가
    - 상황별 HTTP 상태코드 추가
    - 유형에 맞지 않는 입력 값 검증 추가
    - 로그인 검증 미들웨어 추가
    - 비로그인 검증 미들웨어 추가
    - 회원 CRUD 스웨거 API 기능 구현

--- middleware/verifyLoggedIn.middleware.js
+++ middleware/verifyLoggedIn.middleware.js
@@ -1,12 +1,11 @@
 const jwt = require("jsonwebtoken"); // JWT 처리를 위한 모듈
-const { User } = require("../models"); // Sequelize User 모델
+const { User } = require("../models/index.js"); // Sequelize User 모델
 const env = require("../config/env.config.js");
 
 // 사용자 인증 미들웨어
 module.exports = async (req, res, next) => {
   // 요청에서 쿠키를 가져옴
   const { Authorization } = req.cookies;
-  console.log(Authorization, "???");
 
   // 쿠키에서 인증 타입과 토큰을 분리
   const [authType, authToken] = (Authorization ?? "").split(" ");
@@ -25,7 +24,6 @@ module.exports = async (req, res, next) => {
 
     // 사용자 ID로 데이터베이스에서 사용자를 찾음
     const user = await User.findByPk(userId);
-    console.log(user, "들어오긴함?");
 
     // 찾은 사용자 정보를 응답 객체의 로컬 변수에 저장
     res.locals.user = user;
--- routes/user.router.js
+++ routes/user.router.js
@@ -4,38 +4,16 @@ const { User } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const env = require("../config/env.config.js");
-const authenticateToken = require("../middleware/login-middleware.js");
+const verifyLoggedInMiddleware = require("../middleware/verifyLoggedIn.middleware.js");
+const verifyNotLoggedInMiddleware = require("../middleware/verifyNotLoggedIn.middleware.js");
 
-// 회원 가입
-// router.post("/signup", async (req, res) => {
-//   try {
-//     const { email, password, confirmPassword, name } = req.body;
-//     // 비밀번호와 비밀번호 확인이 일치하는지 확인
-//     if (password !== confirmPassword) {
-//       return res.status(400).json({ message: "Passwords do not match" });
-//     }
-//     // 이메일 중복 확인
-//     const existingUser = await User.findOne({ where: { email } });
-//     if (existingUser) {
-//       return res.status(409).json({ message: "Email already in use" });
-//     }
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = await User.create({ email, password: hashedPassword, name });
-//     // 비밀번호를 제외한 사용자 정보 반환
-//     const { password: _, ...userData } = user.toJSON();
-//     res.status(201).json(userData);
-//   } catch (error) {
-//     res.status(400).json(error);
-//   }
-// });
-
-router.post("/signup", async (req, res) => {
+// 회원가입
+router.post("/signup", verifyNotLoggedInMiddleware, async (req, res) => {
+  const { email, password, confirmPassword, name } = req.body;
   try {
-    const { email, password, confirmPassword, name } = req.body;
-
     // 필수 입력 값 검증
     if (!email || !password || !confirmPassword || !name) {
-      return res.status(400).json({ message: "필수 입력 필드가 누락되었습니다" });
+      return res.status(400).json({ message: "필수 입력 정보가 누락되었습니다" });
     }
 
     // 이메일 형식 검증
@@ -46,7 +24,7 @@ router.post("/signup", async (req, res) => {
 
     // 비밀번호 강도 검증
     if (password.length < 8 || !/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[!@#$%^&*]/.test(password)) {
-      return res.status(400).json({ message: "비밀번호가 복잡성 요구 사항을 충족하지 않습니다" });
+      return res.status(400).json({ message: "비밀번호는 최소 8자 이상이며, 대소문자, 숫자, 하나 이상의 특수문자를 포함 해야합니다." });
     }
 
     // 비밀번호 일치 검증
@@ -64,93 +42,108 @@ router.post("/signup", async (req, res) => {
     const user = await User.create({ email, password: hashedPassword, name });
 
     const { password: _, ...userData } = user.toJSON();
-    res.status(201).json(userData);
+    return res.status(201).json(userData);
   } catch (error) {
-    if (error.name === "SequelizeDatabaseError") {
-      return res.status(500).json({ message: "데이터베이스 오류" });
-    }
     console.error(error);
-    res.status(500).json({ message: "내부 서버 오류" });
+    return res.status(500).json({ errorMessage: "내부 서버 오류" });
   }
 });
 
 // 로그인
-router.post("/login", async (req, res) => {
+router.post("/login", verifyNotLoggedInMiddleware, async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
-    // console.log(user, "유저");
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // JWT 토큰 생성
-      const token = jwt.sign({ userId: user.id }, env.JWT_SECRET, { expiresIn: "12h" });
-      res.cookie("Authorization", `Bearer ${token}`);
-      // res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "Strict" });
-      res.status(200).json({ message: "Login successful", token });
-    } else {
-      res.status(401).json({ message: "Invalid credentials" });
+    if (!user) {
+      return res.status(401).json({ message: "존재하지 않는 사용자 입니다." });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "패스워드가 일치하지 않습니다." });
+    }
+
+    const token = jwt.sign({ userId: user.id }, env.JWT_SECRET, { expiresIn: "12h" });
+    res.cookie("Authorization", `Bearer ${token}`);
+    // res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "Strict" });
+    res.status(200).json({ message: "로그인 성공", token });
   } catch (error) {
-    res.status(400).json(error);
+    console.error(error);
+    return res.status(500).json({ errorMessage: "내부 서버 오류" });
   }
 });
 
-// // 내 정보 조회
-// router.get("/me", authenticateToken, async (req, res) => {
-//   try {
-//     const user = await User.findByPk(req.locals.user);
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     const { password, ...userInfo } = user.toJSON();
-//     res.status(200).json(userInfo);
-//   } catch (error) {
-//     res.status(400).json(error);
-//   }
-// });
+// 사용자 조회
+router.get("/user", verifyLoggedInMiddleware, async (req, res) => {
+  try {
+    // 인증 미들웨어를 통해 얻은 사용자 정보를 추출
+    const { email, name } = res.locals.user;
 
-router.get("/me", authenticateToken, async (req, res) => {
-  // 인증 미들웨어를 통해 얻은 사용자 정보를 추출
-  const { email, name } = res.locals.user;
+    // 사용자 정보가 존재하지 않을 경우
+    if (!email || !name) {
+      return res.status(404).json({ message: "사용자 정보를 찾을 수 없습니다" });
+    }
 
-  // 사용자 정보를 반환
-  res.status(200).json({
-    user: { email, name },
-  });
+    // 사용자 정보를 반환
+    res.status(200).json({
+      user: { email, name },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: "내부 서버 오류" });
+  }
 });
 
 // 회원 정보 수정
-router.put("/users/:id", authenticateToken, async (req, res) => {
+router.put("/user", verifyLoggedInMiddleware, async (req, res) => {
+  const { id } = res.locals.user;
+  const { currentPassword, newPassword, name } = req.body;
   try {
-    const { email, name } = req.body;
-    const user = await User.findByPk(req.params.id);
+    const user = await User.findByPk(id);
+
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "해당 사용자를 찾을 수 없습니다." });
     }
-    await user.update({ email, name });
-    res.status(200).json(user);
+
+    const passwordValidation = await bcrypt.compare(currentPassword, user.password);
+    if (!passwordValidation) {
+      return res.status(401).json({ message: "현재 비밀번호가 일치하지 않습니다." });
+    }
+
+    if (newPassword.length < 8 || !/[A-Z]/.test(newPassword) || !/[0-9]/.test(newPassword) || !/[!@#$%^&*]/.test(newPassword)) {
+      return res.status(400).json({ message: "비밀번호는 최소 8자 이상이며, 대소문자, 숫자, 하나 이상의 특수문자를 포함해야 합니다." });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await user.update({ password: hashedPassword, name });
+    res.status(200).json({ message: "사용자 정보가 성공적으로 업데이트되었습니다." });
   } catch (error) {
-    res.status(400).json(error);
+    console.error(error);
+    res.status(500).json({ errorMessage: "내부 서버 오류" });
   }
 });
 
 // 회원 탈퇴
-router.delete("/users/:id", async (req, res) => {
+router.delete("/user", verifyLoggedInMiddleware, async (req, res) => {
+  const { id } = res.locals.user;
   try {
-    const user = await User.findByPk(req.params.id);
+    const user = await User.findByPk(id);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
     }
     await user.destroy();
-    res.status(204).send("회원탈퇴성공");
+    res.clearCookie("Authorization");
+    res.status(204).json({ message: "회원 탈퇴가 성공적으로 처리되었습니다." });
   } catch (error) {
-    res.status(400).json(error);
+    console.error(error);
+    res.status(500).json({ errorMessage: "내부 서버 오류" });
   }
 });
 
 // 로그아웃
-router.post("/logout", (req, res) => {
+router.post("/logout", verifyLoggedInMiddleware, (req, res) => {
   res.clearCookie("Authorization");
-  res.status(200).json({ message: "Logout successful" });
+  res.status(200).json({ message: "로그아웃 성공" });
 });
 
 module.exports = router;
