0ad8af0f01a9713d84978dc583c1ba5a067bd4ea
PachyuChepe <pachyuchepe@gmail.com>
2023-11-17 01:47:00 +0900

    Test: mariaDB 연결 테스트

--- config/db.config.js
+++ config/db.config.js
@@ -13,12 +13,12 @@ module.exports = {
     logging: false, //콘솔창 쿼리 로그 off
   },
 
-  test: {
-    host: process.env.LOCAL_MYSQL_HOST,
-    port: process.env.LOCAL_MYSQL_PORT,
-    username: process.env.LOCAL_MYSQL_USERNAME,
-    password: process.env.LOCAL_MYSQL_PASSWORD,
-    database: process.env.LOCAL_MYSQL_DATABASE_NAME,
+  mariaDB: {
+    host: process.env.CT_MYSQL_HOST,
+    port: process.env.CT_MYSQL_PORT,
+    username: process.env.CT_MYSQL_USERNAME,
+    password: process.env.CT_MYSQL_PASSWORD,
+    database: process.env.CT_MYSQL_DATABASE_NAME,
     dialect: "mysql",
     logging: false,
   },
@@ -34,7 +34,7 @@ module.exports = {
   },
 
   init: function () {
-    const env = process.env.NODE_ENV || "development";
+    const env = process.env.NODE_ENV || "mariaDB";
     const config = this[env];
     return mysql.createConnection({
       host: config.host,
