494076eac7ca90c8ad11c2744da7e44fc224b97a
PachyuChepe <pachyuchepe@gmail.com>
2023-12-05 15:16:14 +0900

    feat: Prisma 모델 생성
    - user & product 모델 생성
    - Prisma 연결 확인 로직 생성

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ const https = require("https");
 const fs = require("fs");
 const YAML = require("yamljs");
 const swaggerUi = require("swagger-ui-express");
+const { checkDatabaseConnection } = require("./config/db.config.js");
 
 // 환경 설정 및 데이터베이스 설정
 const env = require("./config/env.config.js");
-const dbConfig = require("./config/db.config.js");
-
-const conn = dbConfig.init();
-dbConfig.connect(conn);
 
 // 익스프레스 앱 생성 및 설정
 const app = express();
@@ -35,15 +32,13 @@ app.use(
 );
 
 // 라우터 설정
-const userRouter = require("./routes/user.router.js");
-const itemRouter = require("./routes/products.router.js");
+// const userRouter = require("./routes/user.router.js");
+// const itemRouter = require("./routes/products.router.js");
 
-app.use("/", [userRouter, itemRouter]);
+// app.use("/", [userRouter, itemRouter]);
 
 // Swagger API 문서 설정
 const apiSpec = YAML.load("swagger.yaml");
-
-// Swagger API 로컬 서버 포트 변경
 apiSpec.servers = apiSpec.servers.map((server) => {
   if (server.url.includes("localhost")) {
     return {
@@ -61,18 +56,20 @@ app.get("/", (req, res) => {
   res.send("안녕하세요 세계!");
 });
 
-// 서버 생성 및 실행
-let server;
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-
-  const credentials = { key: privateKey, cert: certificate };
-  server = https.createServer(credentials, app);
-  server.listen(env.SERVER_PORT, () => console.log(`HTTPS server is running on port ${env.SERVER_PORT}`));
-} else {
-  server = app.listen(env.SERVER_PORT, () => console.log(`HTTP server is running on port ${env.SERVER_PORT}`));
-}
-
-module.exports = server;
+// 데이터베이스 연결 확인 및 서버 시작
+checkDatabaseConnection().then(() => {
+  let server;
+  if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+    const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+    const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+    const credentials = { key: privateKey, cert: certificate };
+    server = https.createServer(credentials, app);
+    server.listen(env.SERVER_PORT, () => {
+      console.log(`HTTPS server is running on port ${env.SERVER_PORT}`);
+    });
+  } else {
+    server = app.listen(env.SERVER_PORT, () => {
+      console.log(`HTTP server is running on port ${env.SERVER_PORT}`);
+    });
+  }
+});
