89da4ce0d44c5e690a03ebd35d6582402809d10f
PachyuChepe <pachyuchepe@gmail.com>
2023-12-07 11:53:38 +0900

    feat: winston 에러 로깅 추가

--- middleware/errorHandler.middleware.js
+++ middleware/errorHandler.middleware.js
@@ -1,6 +1,28 @@
 // middleware/errorHandler.middleware.js
 
 const ApiError = require("../error/api.error.js");
+const winston = require("winston");
+const { format } = require("logform");
+
+const logFormat = format.combine(
+  format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  format.errors({ stack: true }), // 스택 트레이스 포함
+  format.json(),
+);
+
+const logger = winston.createLogger({
+  level: "error",
+  format: logFormat,
+  transports: [new winston.transports.File({ filename: "error.log", level: "error" })],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: format.simple(),
+    }),
+  );
+}
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof ApiError) {
@@ -8,7 +30,19 @@ const errorHandler = (err, req, res, next) => {
     return;
   }
 
-  console.error(err); // 콘솔 로그가 아닌 에러 로깅 시스템 사용 필요
+  const errorInfo = {
+    message: err.message,
+    stack: err.stack,
+    path: req.path,
+    method: req.method,
+    body: req.body,
+    query: req.query,
+    params: req.params,
+  };
+
+  logger.error(errorInfo);
+  // console.error(err);
+  // 콘솔 로그가 아닌 에러 로깅 시스템 사용 필요
   res.status(500).json({ success: false, message: "서버 오류가 발생했습니다." });
 };
 
--- service/user.service.js
+++ service/user.service.js
@@ -36,7 +36,7 @@ class UserService {
       throw ApiError.Unauthorized("패스워드가 일치하지 않습니다.");
     }
 
-    const accessToken = jwt.sign({ userId: user.id }, env.JWT_SECRET, { expiresIn: "12h" });
+    const accessToken = jwt.sign({ userId: user.id }, env.JWT_SECRET, { expiresIn: "10s" });
     const refreshToken = jwt.sign({ userId: user.id }, env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
     await redisClient.set(user.id.toString(), refreshToken, "EX", 60 * 60 * 24 * 7);
 
