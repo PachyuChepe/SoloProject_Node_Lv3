5c5b05ef6cd68084b12946fca9ecf5d2963c27f4
PachyuChepe <pachyuchepe@gmail.com>
2023-12-08 11:26:14 +0900

    Refactor: 응답 메세지 일관성 있게 변경

--- middleware/productValidation.middleware.js
+++ middleware/productValidation.middleware.js
@@ -7,7 +7,7 @@ const validateCreateProduct = (req, res, next) => {
   const { title, content } = req.body;
 
   if (!title || !content) {
-    throw ApiError.BadRequest("제목과 내용은 필수 항목입니다.");
+    throw ApiError.BadRequest("필수 입력 정보가 누락되었습니다.");
   }
 
   next();
@@ -40,7 +40,7 @@ const validateUpdateProduct = (req, res, next) => {
   const { title, content, status } = req.body;
 
   if (!title || !content || !status) {
-    throw ApiError.BadRequest("제목, 내용, 상태는 필수 항목입니다.");
+    throw ApiError.BadRequest("필수 입력 정보가 누락되었습니다.");
   }
 
   next();
